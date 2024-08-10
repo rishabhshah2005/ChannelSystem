4f091d49355626f1e0d741ac56b198431edb9223
SmitParmar <smitparmar6425@gmail.com>
2024-08-10 12:47:03 +0530

    added settings

--- SQLQueries.java
+++ SQLQueries.java
@@ -1,3 +1,4 @@
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -55,7 +56,88 @@ public class SQLQueries {
     pst.setString(1, username);
     pst.setString(2, password);
     pst.execute();
+  }
 
+  public void fetchcurrentSettings() throws Exception{
+    String sql = "Select * from user";
+    PreparedStatement pst = con.prepareStatement(sql);
+    ResultSet rs = pst.executeQuery();
+    while (rs.next()) {
+      System.out.print("User ID  = "+ rs.getInt(1)+" ");
+      System.out.print("UserName =" + rs.getString(2)+" ");
+      System.out.print("UserPassword = "+rs.getString(3)+" ");
+      System.out.println("Package Id = "+rs.getString(4)+" ");
+    }
+  }
+  //Method to change usernmae
+  public void changeUsername() throws Exception{
+    Statement st = con.createStatement();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the name of the user ");
+            String user = sc.next();
+            System.out.println("Enter the password ");
+            String pass = sc.next();
+            String sql = "select * from  user where username = ? and password = ? ";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, user);
+            pst.setString(2, pass);
+            ResultSet rs = pst.executeQuery();
+            int temp = 0;
+            int pid = 0;
+            while (rs.next()) {
+                temp = 1;
+                System.out.println("Log in success");
+                System.out.println("Welcome " + rs.getInt(1));
+                pid = rs.getInt(1);
+            }
+            if (temp == 1) {
+                System.out.println("ENter new username");
+                String newusername = sc.next();
+                String sqlupdate = "update user set username = '"+newusername+"' where user_id = " + pid;
+                int i = st.executeUpdate(sqlupdate);
+                if (i > 0) {
+                    System.out.println("Update success");
+                } else {
+                    System.out.println("Update failed");
+                }
+            } else {
+                System.out.println("Invalid userid and password");
+            }
+        
+  }
+  //Method to change user password
+  public void changeUserpassword() throws Exception{
+
+    Statement st = con.createStatement();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the name of the user ");
+    String user = sc.next();
+    System.out.println("Enter the old password ");
+    String pass = sc.next();
+
+    String checkSql = "select * from  user where username = '" + user + "' and password = '" + pass + "'";
+    ResultSet rs = st.executeQuery(checkSql);
+    int temp = 0;
+    int pid = 0;
+    while (rs.next()) {
+        temp = 1;
+        System.out.println("Log in success");
+        System.out.println("Welcome " + rs.getInt(1));
+        pid = rs.getInt(1);
+    }
+    if (temp == 1) {
+        System.out.println("ENter new password");
+        String newpass = sc.next();
+        String sql = "update user set password = '" + newpass + "' where user_id = " + pid;
+        int i = st.executeUpdate(sql);
+        if (i > 0) {
+            System.out.println("Update success");
+        } else {
+            System.out.println("Update failed");
+        }
+    } else {
+        System.out.println("Invalid userid and password");
+    }
   }
 
 }
